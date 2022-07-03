c340575cc3643a17b4fc9e7c323b15a63be5bf6a
Mark Harfouche <mark.harfouche@gmail.com>
2022-07-03 13:46:48 -0400

    cleanup a little more

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,7 @@
 
 cd win32
 
-:: Have to set manifest for VS2008.
-:: No idea why, but exe's won't run otherwise.
-set MANIFEST=no
-if "%VS_MAJOR%" == "9" (
-    set MANIFEST=yes
-)
-
 cscript configure.js compiler=msvc iconv=yes icu=no zlib=yes lzma=no python=no ^
-                     vcmanifest=%MANIFEST% ^
                      prefix=%LIBRARY_PREFIX% ^
                      include=%LIBRARY_INC% ^
                      lib=%LIBRARY_LIB%
@@ -22,7 +14,13 @@ if errorlevel 1 exit 1
 nmake /f Makefile.msvc install
 if errorlevel 1 exit 1
 
+:: These programs are listed as "check_PROGRAMS"
+:: Under the unix makefile
+:: https://gitlab.gnome.org/GNOME/libxml2/-/blob/master/Makefile.am
+:: But are always installed for windows ....
+:: https://gitlab.gnome.org/GNOME/libxml2/-/blob/master/win32/Makefile.msvc#L257
 del %LIBRARY_PREFIX%\bin\test*.exe || exit 1
 del %LIBRARY_PREFIX%\bin\runsuite.exe || exit 1
 del %LIBRARY_PREFIX%\bin\runtest.exe || exit 1
+del %LIBRARY_PREFIX%\bin\runxmlconf.exe || exit 1
 copy %LIBRARY_LIB%\libxml2.lib %LIBRARY_LIB%\xml2.lib || exit 1
