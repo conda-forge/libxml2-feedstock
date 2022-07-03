818d72e308cf0c5bf2e73a5f6537a5e4e6d0dc1f
Mark Harfouche <mark.harfouche@gmail.com>
2022-07-03 13:38:42 -0400

    Do not move header files
    Users should use pkg-config to get the correct compilation flags.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -22,11 +22,6 @@ if errorlevel 1 exit 1
 nmake /f Makefile.msvc install
 if errorlevel 1 exit 1
 
-:: For some reason the includes get put down one level.
-move %LIBRARY_INC%\libxml2\libxml %LIBRARY_INC%
-if errorlevel 1 exit 1
-
-rmdir %LIBRARY_INC%\libxml2
 del %LIBRARY_PREFIX%\bin\test*.exe || exit 1
 del %LIBRARY_PREFIX%\bin\runsuite.exe || exit 1
 del %LIBRARY_PREFIX%\bin\runtest.exe || exit 1
