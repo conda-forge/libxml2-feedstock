632c9877222204575ce5baa6a31c07cff7bdcd1e
Chris Burr <chrisburr@users.noreply.github.com>
2023-01-12 15:05:33 +0100

    Allow XML_CATALOG_FILES to be unset

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export xml_catalog_files_libxslt="${XML_CATALOG_FILES}"
+export xml_catalog_files_libxslt="${XML_CATALOG_FILES:-}"
 export XML_CATALOG_FILES="${CONDA_PREFIX}/etc/xml/catalog"
