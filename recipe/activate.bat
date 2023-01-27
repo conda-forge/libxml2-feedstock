98e617679e749a963c2dc46c8e9c5f274fcd6cf3
Johan Hattne <johan@hattne.se>
2023-01-26 18:23:08 -0800

    Prevent all commands from displaying on the screen From https://github.com/conda-forge/libxml2-feedstock/pull/82#issuecomment-1378012606.

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,2 +1,4 @@
+@echo off
+
 set xml_catalog_files_libxml2=%XML_CATALOG_FILES%
 set XML_CATALOG_FILES=%CONDA_PREFIX%\etc\xml\catalog
--- recipe/deactivate.bat
+++ recipe/deactivate.bat
@@ -1,3 +1,5 @@
+@echo off
+
 if defined xml_catalog_files_libxml2 (
     set XML_CATALOG_FILES=%xml_catalog_files_libxml2%
 ) else (
