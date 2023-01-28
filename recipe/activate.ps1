d94514357626a48e0ee4af7e3f22b61935a781bd
Johan Hattne <johan@hattne.se>
2023-01-27 18:42:55 -0800

    Partially URL-encode paths before appending to XML_CATALOG_FILES; add /etc/xml/catalog on Unix Note that activate.ps1 does not appear to be triggered when the package is installed; deactivate.{bat,ps1,sh} do not run when the package is removed.

--- recipe/activate.ps1
+++ recipe/activate.ps1
@@ -1,2 +1,9 @@
-$Env:xml_catalog_files_libxml2 = "$Env:XML_CATALOG_FILES"
-$Env:XML_CATALOG_FILES = "$Env:CONDA_PREFIX\etc\xml\catalog"
+$conda_catalog_files += "file:///" + $Env:CONDA_PREFIX.replace(" ", "%20").replace("\", "/") + "/etc/xml/catalog"
+
+if ($Env:XML_CATALOG_FILES) {
+    $Env:xml_catalog_files_libxml2 = "$Env:XML_CATALOG_FILES"
+    $Env:XML_CATALOG_FILES += " $conda_catalog_files"
+} else {
+    $Env:xml_catalog_files_libxml2 = ""
+    $Env:XML_CATALOG_FILES = "$conda_catalog_files"
+}
--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-export xml_catalog_files_libxml2="${XML_CATALOG_FILES:-}"
-export XML_CATALOG_FILES="${CONDA_PREFIX}/etc/xml/catalog"
+conda_catalog_files="file://${CONDA_PREFIX// /%20}/etc/xml/catalog file:///etc/xml/catalog"
+
+if test -n "${XML_CATALOG_FILES:-}"; then
+    export xml_catalog_files_libxml2="${XML_CATALOG_FILES}"
+    export XML_CATALOG_FILES+=" ${conda_catalog_files}"
+else
+    export xml_catalog_files_libxml2=""
+    export XML_CATALOG_FILES="${conda_catalog_files}"
+fi
