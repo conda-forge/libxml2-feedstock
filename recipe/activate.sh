58dacad396214b502a9ac4d723a8ad8ece8c1b2e
Johan Hattne <johan@hattne.se>
2023-02-03 08:33:40 -0800

    Better implementation consistency between shells

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-conda_catalog_files="file://${CONDA_PREFIX// /%20}/etc/xml/catalog file:///etc/xml/catalog"
-
 if test -n "${XML_CATALOG_FILES:-}"; then
     export xml_catalog_files_libxml2="${XML_CATALOG_FILES}"
-    export XML_CATALOG_FILES+=" ${conda_catalog_files}"
+    XML_CATALOG_FILES+=" "
 else
     export xml_catalog_files_libxml2=""
-    export XML_CATALOG_FILES="${conda_catalog_files}"
+    XML_CATALOG_FILES=""
 fi
+
+conda_catalog_files="file://${CONDA_PREFIX// /%20}/etc/xml/catalog file:///etc/xml/catalog"
+export XML_CATALOG_FILES+="${conda_catalog_files}"
