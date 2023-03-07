9b2727266aa93d52b1b63fda6d50fee35f74ebe3
Johan Hattne <johan@hattne.se>
2023-03-07 11:32:46 -0800

    Avoid += for variable assignment This is not defined in POSIX sh.  See https://github.com/conda-forge/libxml2-feedstock/pull/87#issuecomment-1456302688; thanks to @ocefpaf.

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -2,11 +2,11 @@
 
 if test -n "${XML_CATALOG_FILES:-}"; then
     export xml_catalog_files_libxml2="${XML_CATALOG_FILES}"
-    XML_CATALOG_FILES+=" "
+    XML_CATALOG_FILES="${XML_CATALOG_FILES} "
 else
     export xml_catalog_files_libxml2=""
     XML_CATALOG_FILES=""
 fi
 
 conda_catalog_files="file://${CONDA_PREFIX// /%20}/etc/xml/catalog file:///etc/xml/catalog"
-export XML_CATALOG_FILES+="${conda_catalog_files}"
+export XML_CATALOG_FILES="${XML_CATALOG_FILES}${conda_catalog_files}"
