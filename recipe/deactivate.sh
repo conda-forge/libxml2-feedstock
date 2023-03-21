b98cd64acb62999933100e288762fd275a947045
Johan Hattne <hattne@users.noreply.github.com>
2023-03-21 15:07:03 -0700

    Set and clear XML_CATALOG_FILES on activation and deactivation, respectively (take 4) (#91)
    * Guard against unbound variable; bump build number
    Thanks to @jaimergp.
    
    * use clang_bootstrap compiler on macOS
    
    avoids circular dependency problems when building clang dependencies
    
    * MNT: Re-rendered with conda-build 3.23.3, conda-smithy 3.23.0, and conda-forge-pinning 2023.03.19.10.10.34
    
    * Bump build number
    
    * MNT: Re-rendered with conda-build 3.23.3, conda-smithy 3.23.0, and conda-forge-pinning 2023.03.20.19.31.17
    
    * Bump build number
    
    ---------
    
    Co-authored-by: Min RK <benjaminrk@gmail.com>

--- recipe/deactivate.sh
+++ recipe/deactivate.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-if test -n "${xml_catalog_files_libxml2}"; then
+if test -n "${xml_catalog_files_libxml2:-}"; then
     export XML_CATALOG_FILES="${xml_catalog_files_libxml2}"
 else
     unset XML_CATALOG_FILES
