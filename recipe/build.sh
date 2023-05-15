1be53f000c5df3bc6cce4a8ad1f60173dfd54905
Sam Gillingham <gillingham.sam@gmail.com>
2023-05-15 15:33:20 +1000

    try to force iconv and re-enable tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,11 @@ cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
 ./autogen.sh
 
-# Define TRUE/FALSE via preprocessor flags for now (until upstream fixes it).
-# (Some (non-header) source files use them but not define them or include <stdbool.h> .)
-export CPPFLAGS="${CPPFLAGS} -DFALSE=0 -DTRUE=1"
+if [[ `uname` == "Linux" ]]; then
+  # workaround weird configure behaviour where it decides
+  # it doesn't need libiconv
+  LDFLAGS="-liconv"
+fi
 
 ./configure --prefix="${PREFIX}" \
             --build=${BUILD} \
@@ -20,13 +22,11 @@ export CPPFLAGS="${CPPFLAGS} -DFALSE=0 -DTRUE=1"
             --without-python \
             --enable-static=no
 make -j${CPU_COUNT} ${VERBOSE_AT}
-# Sorry:
-# ## Error cases regression tests
-# file result/errors/759573.xml.err is 1983 bytes, result is 1557 bytes
-# Error for ./test/errors/759573.xml failed
-# if [[ ${target_platform} != osx-64 ]]; then
-#   make check $VERBOSE_AT}
-# fi
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+  make check
+fi
+
 make install
 
 # Remove large documentation files that can take up to 6.6/9.2MB of the install
