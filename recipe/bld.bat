717376a2912a6299e90a4ae64e08685fd55efd41
Sam Gillingham <gillingham.sam@gmail.com>
2025-04-02 17:24:29 +1000

    missing char

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ cd build
 cmake -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
 -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
 -D CMAKE_BUILD_TYPE=Release ^
--D LIBXML2_WITH_ICU=%with_icu%
+-D LIBXML2_WITH_ICU=%with_icu% ^
 -G "NMake Makefiles" ..
 if errorlevel 1 exit 1
 
